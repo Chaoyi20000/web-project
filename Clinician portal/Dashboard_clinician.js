a032fbe5676ce3af28ebe252c2464bddc061635c
Yiya Zhuang <yiya.zhuang@student.unimelb.edu.au>
2022-04-11 18:34:58 +1000

    Dashboard_clinician initialize -no js version

---
+++ Clinician portal/Dashboard_clinician.js
@@ -0,0 +1,9 @@
+var color_PartialComplete = "#E9F3FF"
+var color_Complete = "#DFF1F0"
+var color_Incomplete = "#F0F0F0"
+var color_Abnormal = "#F5E8E5"
+var color_AbnormalBorder = "#FF977E"
+
+var patient_num = 4
+for (var i=0; i<patient_num;i++)
+  $('main').append('<div class="Patient"></div>');
