d19408510fad1465fc3e77693a4882e5146fd7b8
leonaworld <guixianl@student.unimelb.edu.au>
2022-05-19 01:13:46 +1000

    fix small bug in login security

--- routes/loginChecker.js
+++ routes/loginChecker.js
@@ -1,13 +1,13 @@
 // middleware to check patient is logged in
 function notLoggedInPat(req, res, next) {
-    if (req.isAuthenticated()){
+    if (req.isAuthenticated() && req.user.role == "patient"){
       return res.redirect('/home/patient_dashboard');
     }
     next();
   }
 // middleware to check clinician is logged in
 function notLoggedInClin(req, res, next) {
-    if (req.isAuthenticated()){
+    if (req.isAuthenticated() && req.user.role == "clinician" ){
       return res.redirect('/home/clinician_dashboard');
     }
     next();
